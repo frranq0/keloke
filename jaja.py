8b8cdacb1fe356bf7da00957da556b652d2d1ba5
frranq0 <91351705+frranq0@users.noreply.github.com>
2021-09-24 21:09:53 -0300

    Add files via upload

---
+++ jaja.py
@@ -0,0 +1,1 @@
+print ("Hola gran conshesumare")
---
+++ keloke.py
@@ -0,0 +1,18 @@
+print ("***********************************************")
+print("KE PASA EMBARAO E LA CARA")
+print ("***********************************************")
+varNombre = input("Ingrese el nombre    : ")
+print ("***********************************************")
+varApellido = input("Ingrese el apellido    : ")
+print ("***********************************************")
+varEdad = input("Ingrese el edad    : ")
+print ("***********************************************")
+varMontoSolicitud = input("Ingrese el monto solicitao  : ")
+print ("***********************************************")
+varSalario = input("Ingrese el salario    : ")
+print ("***********************************************")
+
+if varMontoSolicitud < varSalario:
+    print("Credito supera el sueldo pela mas la raja")
+else:
+    print("Credito concebido por el ñato el 10%")
